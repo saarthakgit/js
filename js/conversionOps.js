c4d0014349fdef5279710a453119d80748a4dab1
saarthakgit <saarthakgid@gmail.com>
2025-08-18 05:51:53 +0000

    conversion and operations

---
+++ js/conversionOps.js
@@ -0,0 +1,39 @@
+let score;
+// score = "33"
+// console.log(typeof score)
+let convertedScore = Number(score)
+
+
+// conversion:
+// "33" => 33
+// "33abc" => NaN 
+// null => 0
+// undefined => NaN
+// true/false => 1/0
+
+// typeof NaN => number
+
+let IsLoggedIn = undefined
+let Bool = Boolean(IsLoggedIn)
+// console.log(Bool)
+// console.log(typeof Bool)
+
+// 1 and any other value which is not ZERO or NULL or undefined or a blank => true, else false.
+
+// ===========OPERATIONS============
+
+let value = 5
+let neg = -value
+// console.log(neg);
+
+// console.log("1"+1)
+// console.log("1"+1)
+// console.log("1"+1+2)
+// console.log(1+1+20+"2")
+
+// ***execution from left to right
+
+// PREFIX : returns value after executing operation
+// POSTFIX: returns value before operation
+
+
