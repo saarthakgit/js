f4744bbdeb8929b5b777366464b914f4c0884ed5
saarthakgit <saarthakgid@gmail.com>
2025-08-18 06:14:20 +0000

    comparisions

---
+++ js/comparison.js
@@ -0,0 +1,20 @@
+// console.log("2">1); => true
+// console.log("02">1); => true
+// console.log(null>0); => false
+// console.log(null==0); =>false
+// console.log(0==null); =>false
+// console.log(null==undefined); =>true
+// console.log(undefined == 0); => false
+// console.log(""==false); => true
+// console.log("1"==true); =>true
+// console.log(null<undefined); =>false
+
+// NULL:
+// console.log(null==0); => FALSE
+// console.log(null>0); => False
+// console.log(null>=0 && null<=0) => ***TRUE*** because inequality and equality checks work differently. Inequality checks convert null to a number (0), whereas equality does not!
+
+// strict check ===
+// console.log(null===0); => FALSE
+// console.log("2"===2); => FALSE
+
