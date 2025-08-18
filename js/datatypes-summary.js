88d98046139566aa557662b147f4e773f7f56180
saarthakgit <saarthakgid@gmail.com>
2025-08-18 08:25:23 +0000

    summary of datatypes and js theory

---
+++ js/datatypes-summary.js
@@ -0,0 +1,15 @@
+// Data is categorized into two categories on the basis of how it is accessed and stored in the memory-- Primitive and Non Primitive (or Reference)
+
+// Primitive- 7 types
+// String, numbers, boolean, symbol, BigInt,null, undefined
+
+// Non Primitive (Reference)
+// Array, Objects, Functions
+
+// -- Javascript is a Dynamically typed language 
+// type of a variable is determined at runtime, we dont need to explicitly define the datatype for variable & variable can hold values of different type by reassigning it.
+
+// DataType of null is OBJECT.
+// DataType of Non Primitive is Object: (FunctionObject for functions)
+// Undefined is a datatype itself
+
