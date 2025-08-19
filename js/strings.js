479f2db257ee5a65e76ded866897c1dc6119fbef
saarthakgit <saarthakgid@gmail.com>
2025-08-19 08:11:12 +0000

    strings and some methods

---
+++ js/strings.js
@@ -0,0 +1,15 @@
+// const name = "   sarthak   "
+// const score = "100"
+// // console.log(name+score)
+// // better way to use strings-backticks
+// console.log(`We can use backticks which is a better option to use a string ${name}:${score}`);
+
+// console.log(name.toUpperCase())
+// console.log(name.charAt(6))
+// console.log(name.indexOf("s"));
+// console.log(name.trim())
+// const newname=name.replace("s","S")
+// console.log(newname);
+const cars = "civic-cruze-polo-vento-rapid"
+let carlist=cars.split("-")
+console.log(carlist[2])
