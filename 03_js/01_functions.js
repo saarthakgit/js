41f0a5227b32849a157cff4fb84a820f2d68558d
saarthakgit <saarthakgid@gmail.com>
2025-08-21 10:57:05 +0000

    functions

--- 03_js/01_functions.js
+++ 03_js/01_functions.js
@@ -21,4 +21,35 @@ console.log(greeting("sarthak"))
 
 // if you pass "no argument" to a function which asks for parameter, then it is taken as undefined. (unless default is specified)
 
-console.log(greeting());
+// console.log(greeting());
+
+// shopping cart problem (when we do not know what number of arguments will actually be passed)
+
+function cartPrice(...num1){
+    return num1
+}
+// console.log(cartPrice(200,300,500));
+// "..." is called REST and SPREAD operator both, depending on the usecase.
+// here, it is rest, the function returns an array of input values.
+
+// Another method
+
+function calcPrice(val1,val2,...num1){
+    return num1
+}
+// console.log(calcPrice(200,300,500,1000,2000));
+// here, first two values are taken up by val1 and val2, and rest are added to the array
+
+// object handling
+ function Objecthandler(para_object){
+    // console.log(`Hello, this is object handled! by-${para_object.name},class-${para_object.class}`);
+ }
+Objecthandler({name:"Sarthak",class:"X-A"})
+
+
+// Array handling
+const arr = [1,2,3,4,5,9]
+function secondval(arr_name){
+    return arr_name[1]
+}
+// console.log(secondval(arr));
